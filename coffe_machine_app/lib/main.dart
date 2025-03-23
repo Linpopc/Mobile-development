d4451f0429341dc882d2dc30755a6a8f8483f57a
Linpopc <i_avanesov@bk.ru>
2025-03-23 18:59:51 +0300

    refactor: main

--- coffe_machine_app/lib/main.dart
+++ coffe_machine_app/lib/main.dart
@@ -1,8 +1,18 @@
 import 'classes/Machine.dart';
+import 'classes/Enums.dart';
 
-void main(){
-  Machine coffeeMachine = Machine(100, 500, 1000, 0);
-  coffeeMachine.makingCoffee();
-  coffeeMachine.makingCoffee();
+void main() {
+
+  Machine coffeeMachine = Machine(
+      Resources(coffeeBeans: 200, milk: 500, water: 1000, cash: 500));
+
+
+  coffeeMachine.makingCoffeeByType(coffeType.latte);
+  coffeeMachine.makingCoffeeByType(coffeType.cappuccino);
+  coffeeMachine.makingCoffeeByType(coffeType.espresso);
+
+
+  coffeeMachine.fillResources(100, 200, 300, 50);
+
 }
 
